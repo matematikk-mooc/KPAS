096751a2c38316730034ff815f2c281e19e579f6
etsikt <erlend.thune@gmail.com>
2019-02-12 08:52:00 +0100

    Initial version.

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+ini_set('display_errors', 1);
+session_start();
+require_once 'vendor/autoload.php';
+require_once 'kpasinc/dataporten.inc';
+require_once 'kpasinc/vars.inc';
+require_once 'kpasinc/canvas.inc';
+require_once 'kpasinc/utility.inc';
+
+use Kasperrt\OAuth2;
+
+$oauth2 = null;
+$initialized = false;
+
+$userInfo = "";
+
+if(isset($_GET['course_id'])) {
+    $course_id = $_GET['course_id'];
+    $_SESSION["courseId"] = $course_id;
+}
+else if(!$_SESSION["courseId"])
+{
+    echo "Mangler course_id parameter.";
+    exit; 
+}
+
+if(!$offline)
+{
+    $oauth2 = new OAuth2([
+        "client_id"          => $client_id,
+        "client_secret"      => $client_secret,
+        "redirect_uri"       => $redirect_uri,
+        "auth"               => "https://auth.dataporten.no/oauth/authorization",
+        "token"              => "https://auth.dataporten.no/oauth/token",
+        "response_type" 	 => "code",
+        "session"			 => true
+    ]);
+    if(isset($_GET['code'])) {
+        $code = $_GET['code'];
+        $state = $_GET['state'];
+        mydbg("Code:" . $code);
+        mydbg("State:" . $state);
+        mydbg("Getting access token...");
+        $access_token = $oauth2->get_access_token($state, $code);
+        mydbg($access_token);
+        $_SESSION["token"] = $access_token;	
+        $userInfo = getUserInfoFromDataporten($access_token, $oauth2);
+        $groupsInfo = getGroupsInfoFromDataporten($access_token, $oauth2);
+        $extraUserInfo = getExtraUserInfoFromDataporten($access_token, $oauth2);
+
+        $initialized = true;
+    } else if(isset($_GET['logout'])) {
+        session_destroy();
+        header('HTTP/1.1 302 Found');
+        header('Location: https://auth.dataporten.no/logout');
+    }else {
+        $oauth2->redirect();
+    }
+}
+else
+{
+    $userInfo = json_decode($offlineUserInfo,true);
+    $groupsInfo = json_decode($offlineGroupsInfo,true);
+    $extraUserInfo = json_decode($offlineExtraUserInfo,true);
+    $initialized = true;
+}
+
+if($initialized)
+{
+    $_SESSION["userInfo"] = $userInfo;
+    $_SESSION["groups"] = $groupsInfo;
+    $_SESSION["extraUserInfo"] = $extraUserInfo;
+	header('Location: worker.php');
+}
+?>
---
+++ worker.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+//require_once 'vendor/autoload.php';
+require_once 'kpasinc/dataporten.inc';
+require_once 'kpasinc/vars.inc';
+require_once 'kpasinc/canvas.inc';
+require_once 'kpasinc/utility.inc';
+
+printHtmlHeader();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $group = $_POST["group"];
+    $groupCategoryId = $_POST["groupCategoryId"];
+    $userId = $_POST["userId"];
+    echo "Melder deg inn i " . $group;
+    echo "<br/>GruppekategoriId: ". $groupCategoryId;
+    echo "<br/>BrukerId: " . $userId;
+    AddUserToGroup($userId, $groupCategoryId, $group);
+}
+printDataportenHeading();
+$courseId = $_SESSION["courseId"];
+printCourseId($courseId);
+$userInfo = $_SESSION["userInfo"];
+$dataportenGroups = $_SESSION["groups"];
+$extraUserInfo = $_SESSION["extraUserInfo"];
+printDataportenUserInfo($userInfo);
+printDataportenExtraUserInfo($extraUserInfo);
+printDataportenGroupsInfo($dataportenGroups);
+printCanvasHeading();
+$feideid = getFeideIdFromDataportenUserInfo($userInfo);
+$canvasUser = null;
+if($offline)
+{
+    $canvasUser = json_decode($offlineCanvasUser,true);
+}
+else
+{
+    $canvasUser = getCanvasUserFromFeideId($feideid);
+}
+printCanvasUser($canvasUser);
+$user_id = getCanvasUserIdFromCanvasUser($canvasUser);
+printCanvasUserId($user_id);
+//$canvasEnrollments = getCanvasEnrollments($user_id);
+//printCanvasEnrollments($canvasEnrollments);
+//printCanvasEnrollmentGroups($user_id, $canvasEnrollments, $dataportenGroups);
+printGroupsForCourse($user_id, $courseId, $dataportenGroups);
+//    printCommonGroups($dataportenGroups, $canvasEnrollmentGroups);
+printHtmlFooter();
+
+?>
